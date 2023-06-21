9d13b1f9b1ccd084b9c9c56ce7fe41b86f7f435e
Diogo Valente <diogovalente@hotmail.com.br>
2023-06-21 16:51:03 -0300

    Criação do projeto tech challenge da fiap

---
+++ src/main/java/br/com/fiap/powersave/PowersaveApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.powersave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowersaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowersaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/powersave/PowersaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.powersave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowersaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
